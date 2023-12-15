6f1da72de81f30b67659eead5ba6818424bb4455
arb sht <shtjefni.arbi@iisgalvanimi.edu.it>
2023-12-15 10:45:07 +0000

    commit esercizio

--- introduzione-angular/shtj-test/src/app/app.module.ts
+++ introduzione-angular/shtj-test/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { SecondComponentComponent } from './second-component/second-component.component';
 import { ThirdComponentComponent } from './third-component/third-component.component';
+import { FourthComponentComponent } from './fourth-component/fourth-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SecondComponentComponent,
-    ThirdComponentComponent
+    ThirdComponentComponent,
+    FourthComponentComponent
   ],
   imports: [
     BrowserModule
