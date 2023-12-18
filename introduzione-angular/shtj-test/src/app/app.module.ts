7c89759a936ea53be3d020c7411d77f0737e66a0
arb sht <shtjefni.arbi@iisgalvanimi.edu.it>
2023-12-18 13:58:01 +0000

    aggiunta quinto componente con ngfor

--- introduzione-angular/shtj-test/src/app/app.module.ts
+++ introduzione-angular/shtj-test/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { SecondComponentComponent } from './second-component/second-component.component';
 import { ThirdComponentComponent } from './third-component/third-component.component';
 import { FourthComponentComponent } from './fourth-component/fourth-component.component';
+import { FifthComponentComponent } from './fifth-component/fifth-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SecondComponentComponent,
     ThirdComponentComponent,
-    FourthComponentComponent
+    FourthComponentComponent,
+    FifthComponentComponent
   ],
   imports: [
     BrowserModule
